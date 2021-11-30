e504b2770abc9c49d6957275eadd65d476ee2adf
vagrant <Ola Daniel>
2021-11-30 17:29:34 +0000

    code cleanup

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,24 @@
 const request = require('request');
 
-var myArgs = process.argv.slice(2);
-var breed = myArgs[0];
+const myArgs = process.argv.slice(2);
+const breed = myArgs[0];
 
 const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-request(url+breed, (error, response, body) => {
+request(url + breed, (error, response, body) => {
 
   if (error) {
     console.log('error:', error);
     return;
   }
 
-  console.log('statusCode:', response && response.statusCode);
+  //console.log('statusCode:', response && response.statusCode);
   
-  //console.log('body:', body);
-  //console.log(`typeof body:`, typeof body);
-
   const data = JSON.parse(body);
-  //console.log(data);
-  //console.log('typeof data: ', typeof data);
 
   if (data.length > 0) {
     console.log("description: ", data[0]["description"]);
   } else {
-    console.log(`${breed} not found, please check the name and try again.`)
+    console.log(`${breed} not found, please check the name and try again.`);
   }
 });
