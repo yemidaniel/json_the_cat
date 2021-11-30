5f8c925adff881af7a55c5f592799655de22cd80
vagrant <Ola Daniel>
2021-11-30 16:42:21 +0000

    get breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,15 @@ const request = require('request');
 const url = "https://api.thecatapi.com/v1/breeds/search?q=siberian";
 
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  
+  //console.log('body:', body);
+  //console.log(`typeof body:`, typeof body);
+
+  const data = JSON.parse(body);
+  //console.log(data);
+  //console.log('typeof data: ', typeof data);
+
+  console.log("description: ", data[0]["description"]);
 });
